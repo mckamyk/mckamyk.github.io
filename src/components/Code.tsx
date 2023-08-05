d3e4d746de7e015bc1c1a51023072b8e77828cd8
McKittrick Kaminski <mckittrick.kaminski@gmail.com>
2023-08-04 22:00:27 -0500

    Seems to be an issue with react-syntax-highlighter and Astro

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,18 +1,23 @@
-import SyntaxHighligher from 'react-syntax-highlighter'
-import {tomorrowNightBlue} from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+// import SyntaxHighligher from 'react-syntax-highlighter'
+// import {tomorrowNightBlue} from 'react-syntax-highlighter/src/styles/hljs'
 
 interface Props {
 	code: string;
 	filename?: string;
 }
 
-export default function Code({code, filename}: Props) {
+const Code = ({code, filename}: Props) => {
 	return (
 		<div className='w-full shadow-sky-950 shadow-lg '>
 			{filename && <div className="bg-blue-950 px-2 py-1 rounded-t text-slate-400">{filename}</div>}
-			<SyntaxHighligher language="typescript" style={tomorrowNightBlue} className={`${filename ? "rounded-b" : "rounded"} w-full`}>
-				{code.trim()}
-			</SyntaxHighligher>
+      <code>
+        {code.trim()}
+      </code>
+			{/* <SyntaxHighligher language="typescript" style={tomorrowNightBlue} className={`${filename ? "rounded-b" : "rounded"} w-full`}> */}
+			{/* 	{code.trim()} */}
+			{/* </SyntaxHighligher> */}
 		</div>
 	)
 }
+
+export default Code
